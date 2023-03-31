70ee3e4ac050b2e29d38c2e313cccfb8806ed4f1
Audrey LOURI <audrey.louri@gmail.com>
2023-03-31 11:31:47 +0200

    add search bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   import HeaderApp from './components/HeaderApp.vue';
+  import SearchBar from './components/SearchBar.vue';
 </script>
 
 <template>
-  <div class="h-screen flex mx-[355px] items-center bg-gray-100">
+  <div class="h-screen flex flex-col px-[355px] items-center bg-blue-100">
     <HeaderApp />
+    <SearchBar />
   </div>
 </template>
 
